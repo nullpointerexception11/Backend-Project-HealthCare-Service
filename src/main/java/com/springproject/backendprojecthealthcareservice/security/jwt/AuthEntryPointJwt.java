b4e29cf7816f9901fdc79ee40056f22f1ff7286a
lostcode <lostcoding11@gmail.com>
2022-03-10 14:52:05 +0300

    security service introduced v2

--- src/main/java/com/springproject/backendprojecthealthcareservice/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/springproject/backendprojecthealthcareservice/security/jwt/AuthEntryPointJwt.java
@@ -22,7 +22,4 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
         logger.error("Unauthorized error: {}", authException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
     }
-=======
-public class AuthEntryPointJwt {
->>>>>>> origin/master
 }
