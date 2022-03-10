432b5e6b97fb692327fbcd1bafac7c6ab602a6ef
lostcode <lostcoding11@gmail.com>
2022-03-10 14:52:05 +0300

    security service introduced v2

--- src/main/java/com/springproject/backendprojecthealthcareservice/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/springproject/backendprojecthealthcareservice/security/jwt/AuthEntryPointJwt.java
@@ -1,16 +1,17 @@
 package com.springproject.backendprojecthealthcareservice.security.jwt;
 
-<<<<<<< HEAD
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
@@ -22,7 +23,4 @@ public class AuthEntryPointJwt implements AuthenticationEntryPoint {
         logger.error("Unauthorized error: {}", authException.getMessage());
         response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Error: Unauthorized");
     }
-=======
-public class AuthEntryPointJwt {
->>>>>>> origin/master
 }
