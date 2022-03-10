d860848ee4a199c9dda1f9ac670160cc472bb59f
lostcode <lostcoding11@gmail.com>
2022-03-10 13:00:44 +0300

    v1

---
+++ src/main/java/com/springproject/backendprojecthealthcareservice/BackendProjectHealthCareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.backendprojecthealthcareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectHealthCareServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendProjectHealthCareServiceApplication.class, args);
+    }
+
+}
