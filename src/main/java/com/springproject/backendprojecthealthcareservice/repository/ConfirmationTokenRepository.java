502e2977e824c1a7fb782891eb5bb1ccaf5deba8
lostcode <lostcoding11@gmail.com>
2022-03-13 22:11:59 +0300

    in ConfirmationTokenRepository bug fixed

--- src/main/java/com/springproject/backendprojecthealthcareservice/repository/ConfirmationTokenRepository.java
+++ src/main/java/com/springproject/backendprojecthealthcareservice/repository/ConfirmationTokenRepository.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationTokenRepository, Long> {
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken, Long> {
 
     Optional<ConfirmationToken> findByToken(String token) throws ResourceNotFoundException;
 
