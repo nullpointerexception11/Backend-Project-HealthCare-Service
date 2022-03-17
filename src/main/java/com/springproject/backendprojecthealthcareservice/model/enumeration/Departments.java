d5fbac9d299b15a59d594aeadce1a16f5a92a665
lostcode <lostcoding11@gmail.com>
2022-03-17 19:19:15 +0300

    Department created

---
+++ src/main/java/com/springproject/backendprojecthealthcareservice/model/enumeration/Departments.java
@@ -0,0 +1,7 @@
+package com.springproject.backendprojecthealthcareservice.model.enumeration;
+
+public enum Departments {
+
+    Neurology, NutritionAndDietetics, ObstetricsAndGynecology, Oncology, Orthopaedics, Otolaryngology, Physiotherapy,
+    Radiology, Radiotherapy, Urology, Cardiology, Gastroenterology, GeneralSurgery, Gynecology, Haematology
+}
